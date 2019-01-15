f52649e6b128e5f6c94975df94944abb9106a905
fanyushun <723107234@qq.com>
2019-01-15 19:30:58 +0800

    add router code

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import store from './store'
 Vue.use(MintUI)
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  console.log(to)
+  console.log(from)
+  console.log(next)
+})
+
 new Vue({
   router,
   store,
